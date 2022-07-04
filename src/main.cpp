3310d777ce0521943f27c9630f2dcd800db430fb
wicia98 <wicia98@gmail.com>
2022-07-04 15:05:34 +0200

    some update

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,28 @@
 
 using namespace std;
 
+typedef struct{
+    int ID;
+    string Lang1;
+    string Lang2;
+    int points;
+}Record;
+
 int main()
 {
-    fstream sheet;
-    sheet.open("data/Test.csv");
+    int x;
+    cout<<"Podaj liczbe wyrazow do nauki"<<endl;
+    cin>>x;
+    if(x != 0)
+        Record record[x];
+    FILE* sheet;
+    sheet = fopen("C:\\Users\\mwit\\Documents\\projects\\flashcards\\data\\Test.csv", "r"); 
+
+    string line;
+    while(getline(sheet, line))
+    {
+        
+    }
 
     sheet.close();
     return 0;
